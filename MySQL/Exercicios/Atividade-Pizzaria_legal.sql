596eaf7fb416c8eb645087fa2da3069324fbb2b3
Vitória Santos Oliveira <contato.santos.vitoria12@gmail.com>
2022-01-14 10:36:47 -0300

    adicionando exercicios

--- MySQL/Exercicios/Atividade-Pizzaria_legal.sql
+++ MySQL/Exercicios/Atividade-Pizzaria_legal.sql
@@ -17,3 +17,22 @@ use db_pizzaria_legal;
 alter table tb_categoria add categoria_id bigint;
 alter table tb_categoria drop categoria_id;
 
+insert into tb_categoria (categoria,tamanho) values ("doce","4 pedaços");
+insert into tb_categoria (categoria,tamanho) values ("doce","8 pedaços");
+insert into tb_categoria (categoria,tamanho) values ("salgada","4 pedaços");
+insert into tb_categoria (categoria,tamanho) values ("salgada","8 pedaços");
+
+insert into tb_pizza (pizza,descricao,valor,categoria_id) values ("mussarela","queijo mussarela, azeitona e orégano",39.00,4);
+insert into tb_pizza (pizza,descricao,valor,categoria_id) values ("mussarela","queijo mussarela, azeitona e orégano",24.50,3);
+insert into tb_pizza (pizza,descricao,valor,categoria_id) values ("nutella","nutela com morangos",42.30,2);
+insert into tb_pizza (pizza,descricao,valor,categoria_id) values ("nutella","nutela com morangos",30.00,1);
+insert into tb_pizza (pizza,descricao,valor,categoria_id) values ("caipira","frango desfiado, catupiry e milho verde",39.00,4);
+insert into tb_pizza (pizza,descricao,valor,categoria_id) values ("caipira","frango desfiado, catupiry e milho verde",24.00,3);
+insert into tb_pizza (pizza,descricao,valor,categoria_id) values ("banana","banana com chocolate",42.30,2);
+insert into tb_pizza (pizza,descricao,valor,categoria_id) values ("banana","banana com chocolate",30.00,1);
+
+select * from tb_pizza where valor>45.00;
+select * from tb_pizza where valor between 29.00 and 60.00;
+select * from tb_pizza where pizza like "%c%";
+
+select * from tb_pizza inner join tb_categoria on tb_categoria.id = tb_pizza.categoria_id;
